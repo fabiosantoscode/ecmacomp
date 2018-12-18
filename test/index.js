11a6d5c3d89e8be9bd51f333a54415b6362f4a49
Fábio Santos <fabiosantosart@gmail.com>
2018-12-18 19:12:49 +0000

    WIP

--- test/index.js
+++ test/index.js
@@ -2,10 +2,9 @@
 
 const assert = require('assert').strict || require('assert')
 const jscomp = require('..')
-const parser = require('../lib/parser')
 
 describe('jscomp', () => {
-  /*it('compiles Javascript to bytecode', () => {
+  it('compiles Javascript to bytecode', () => {
     assert.deepEqual(jscomp(`
       function foo() {
         bar(4)
@@ -13,7 +12,7 @@ describe('jscomp', () => {
       foo()
       const x = 6
     `), [
-      ['function', ['name', 'foo']],
+      ['fn', ['name', 'foo']],
       ['push', 4],
       ['push', ['name', 'bar']],
       ['call', 1],
@@ -21,22 +20,92 @@ describe('jscomp', () => {
       ['push', ['name', 'foo']],
       ['call', 0],
       ['push', 6],
-      ['const', 'x'],
+      ['var', 'const', 'x'],
       ['end']
     ])
-  })*/
-  it('compiles javascript to bytecode', () => {
-    assert.deepEqual(jscomp.stringify([
-      ['function', ['name', 'foo']],
-      ['push', 4],
+  })
+  it('compiles more javascript to bytecode', () => {
+    assert.deepEqual(jscomp(`
+      if (foo) {
+        bar()
+      } else {
+        baz()
+      }
+      try {
+        foo()
+      } catch(e) {
+        baz()
+      }
+      let x = 3 ? 1 : 6
+      var fn = ({ a: [...f] }) => f
+      const otherFn = ({foo: bar}) => {
+        foo()
+      }
+      switch (something) {
+        case 0:
+        default:
+          break;
+      }
+      do {
+        foo()
+      } while(bar)
+      while (baz) {
+        foo()
+      }
+    `), [
+      ['fn'],
+      ['push', ['name', 'baz']],
+      ['call', 0],
+      ['end'],
+      ['fn'],
       ['push', ['name', 'bar']],
-      ['call', 1],
+      ['call', 0],
+      ['end'],
+      ['push', ['name', 'foo']],
+      ['push', ['name', ':if']],
+      ['call', 9],
+      ['fn'],
+      ['push', ['name', 'baz']],
+      ['call', 0],
+      ['end'],
+      ['fn'],
+      ['push', ['name', 'foo']],
+      ['call', 0],
+      ['end'],
+      ['push', ['name', 'e']],
+      ['push', ['name', ':try']],
+      ['call', 9],
+      ['fn'],
+      ['push', 3],
+      ['end'],
+      ['fn'],
+      ['push', 1],
+      ['end'],
+      ['fn'],
+      ['push', 6],
+      ['end'],
+      ['push', ['name', ':tern']],
+      ['call', 9],
+      ['var', 'let', 'x']
+    ])
+  })
+  it('compiles bytecode to Javascript', () => {
+    assert.deepEqual(jscomp.stringify([
+      ['fn', ['name', 'foo']],
+      ['push', 4],
+      ['fn', '=>'],
+      ['ret', 3],
+      ['end'],
+      ['push', 'bar'],
+      ['push', { foo: 1 }],
+      ['push', ['name', 'bar']],
+      ['call', 6],
       ['end'],
       ['push', ['name', 'foo']],
       ['call', 0],
       ['push', 6],
-      ['const', 'x'],
+      ['var', 'const', 'x'],
       ['end']
-    ]), 'function foo(){bar(4);}foo();const x=6;')
+    ]), 'fn foo(){bar(4,()=>{return 3},{foo:"bar"});}foo();const x=6;')
   })
 })
