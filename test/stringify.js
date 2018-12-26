3a9fe67254278bb81034fc0adc3efc2cc36f5d97
Fábio Santos <fabiosantosart@gmail.com>
2018-12-26 22:51:55 +0000

    more stringify work

--- test/stringify.js
+++ test/stringify.js
@@ -12,7 +12,7 @@ describe('stringify', () => {
         ['push', ['name', 'fn']],
         ['call', 2]
       ]),
-      'fn(3,i);'
+      'fn(3,i)'
     )
   })
   it('can use spread', () => {
@@ -21,15 +21,15 @@ describe('stringify', () => {
         ['push', ['name', 'a'], {spread: 1}],
         ['push', [0]],
         ['push', ['name', 'foo']],
-        ['call', 2]
+        ['call', 1]
       ]),
-      'foo([...a]);'
+      'foo([...a])'
     )
   })
   it('can read a function', () => {
     assert.equal(
       ecmacomp.stringify([
-        ['fn', ['name', 'foo'], ['name', 'a', { spread: 1 }]],
+        ['fn', 'foo', ['name', 'a', { spread: 1 }]],
         ['push', 3],
         ['ret'],
         ['end']
@@ -38,7 +38,7 @@ describe('stringify', () => {
     )
     assert.equal(
       ecmacomp.stringify([
-        ['fn', ['name', 'foo'], ['name', 'a', { def: [['push', 4]] }], ['array'], ['name', 'b'], ['name', 'c', { spread: 1 }], ['end'], ['object'], ['name', 'foo', { def: [['push', 'bar']] }], ['end']],
+        ['fn', 'foo', ['name', 'a', { def: [['push', 4]] }], ['array'], ['name', 'b'], ['name', 'c', { spread: 1 }], ['end'], ['object'], ['name', 'foo', { def: [['push', 'bar']] }], ['end']],
         ['push', 3],
         ['ret'],
         ['end']
@@ -46,9 +46,9 @@ describe('stringify', () => {
       'function foo(a=4,[b,...c],{foo="bar"}){return 3}'
     )
   })
-  it.skip/*TODO*/('compiles bytecode to Javascript', () => {
+  it('compiles bytecode to Javascript', () => {
     assert.deepEqual(ecmacomp.stringify([
-      ['fn', ['name', 'foo']],
+      ['fn', 'foo'],
       ['push', ['name', 'i']],
       ['push', ['name', ':decr']],
       ['call', 1],
@@ -57,14 +57,14 @@ describe('stringify', () => {
       ['push', ['name', ':decrby']],
       ['call', 2],
       ['push', 'bar'],
-      ['push', { foo: 1 }],
-      ['fn', null],
+      ['push', { foo: 0 }],
+      ['fn', '=>'],
       ['push', 3],
       ['ret'],
       ['end'],
       ['push', 4],
       ['push', ['name', 'bar']],
-      ['call', 6],
+      ['call', 3],
       ['end']
       /*
        * TODO
@@ -73,7 +73,7 @@ describe('stringify', () => {
       ['push', 6],
       ['var', 'const', 'x']
       */
-    ], {debug:1}), 'function foo(){i--;i-=1;bar(4,()=>{return 3},{foo:"bar"})}') // foo();const x=6;')
+    ]), 'function foo(){i--;i-=1;bar(4,()=>{return 3},{"foo":"bar"})}') // foo();const x=6;')
   })
   it('can beautify the code if asked to')
 })
